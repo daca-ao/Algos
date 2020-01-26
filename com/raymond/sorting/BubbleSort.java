86bf55c53d534cc13c5d8855d4b5b82fbbb9c48c
AO Huijun <daca.hjao@gmail.com>
2020-01-26 18:58:25 +0800

    enhance Bubble sort

--- com/raymond/sorting/BubbleSort.java
+++ com/raymond/sorting/BubbleSort.java
@@ -11,10 +11,29 @@ public class BubbleSort {
         }
     }
 
+    public static void sortE(int[] a) {
+        final int n = a.length;
+        boolean needSwap = true;
+        for (int i = 0; i < n - 1 && needSwap; i++) {
+            needSwap = false;
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (a[j] > a[j + 1]) {
+                    Swapper.swap(a, j, j + 1);
+                    needSwap = true;
+                }
+            }
+        }
+    }
+
     public static void main(String[] args) {
         int[] b = { 3, 6, 12, 9, 0, 4, 1, 21 };
-        BubbleSort.sort(b);
+        sort(b);
         for (final int i : b) {
+            System.out.println(i);
+        }
+        int[] c = { 3, 6, 12, 9, 0, 4, 1, 21 };
+        sortE(c);
+        for (final int i : c) {
             System.out.println(i);
         }
     }
